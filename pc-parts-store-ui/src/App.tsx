6d94b19056cc99f84c45d4232fd1e8a52aa9f074
Craig Fox <craig.fox@icloud.com>
2026-07-15 18:07:22 +1200

    Implemented navbar

--- pc-parts-store-ui/src/App.tsx
+++ pc-parts-store-ui/src/App.tsx
@@ -1,15 +1,21 @@
 import Navbar from "./components/Navbar";
 
 function App() {
-    return (
-        <>
-            <Navbar />
+  return (
+    <div className="min-h-screen bg-slate-100">
+      <Navbar />
 
-            <main>
-                <h1>Welcome!</h1>
-            </main>
-        </>
-    );
+      <main className="mx-auto max-w-7xl p-8">
+        <h1 className="text-4xl font-bold">
+          Welcome to the PC Parts Store
+        </h1>
+
+        <p className="mt-4 text-lg text-slate-600">
+          Build your dream PC from our range of quality components.
+        </p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
